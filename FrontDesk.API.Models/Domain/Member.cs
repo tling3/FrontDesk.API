0e183cc693c66c23345d78d8e537379cc53afba8
Tim Ling <tling3@yahoo.com>
2020-05-30 17:03:22 -0700

    added models project with member domain model

---
+++ FrontDesk.API.Models/Domain/Member.cs
@@ -0,0 +1,11 @@
+﻿using FrontDesk.API.Models.Request.Update;
+using System;
+
+namespace FrontDesk.API.Models.Domain
+{
+    public class Member : MemberUpdateRequest
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ FrontDesk.API.Models/Request/Update/MemberUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿using FrontDesk.API.Models.Request.Add;
+
+namespace FrontDesk.API.Models.Request.Update
+{
+    public class MemberUpdateRequest : MemberAddRequest
+    {
+        public int Id { get; set; }
+    }
+}
